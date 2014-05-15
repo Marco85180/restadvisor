157b0830ff2458bf2bd209e2147c242801a4f759
MARCO <lefollmarc@hotmail.fr>
2014-05-15 12:24:34 +0200

    Project rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restadvisor::Application.config.secret_key_base = '4739511a71879f1bbc615f95a4cee3f962fc3a535238f40cba7fa5cf1d9fb4c1929964896a357373260556e5461dbf2fbe45aedc79dbd4c4ce736cc30b14810d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Restadvisor::Application.config.session_store :cookie_store, key: '_restadvisor_session'
